9a56da56ef98ff613c7f2a6bc60a2b2ade9e8b38
VickerTan <vickerpangestan@gmail.com>
2023-07-04 15:09:56 +0800

    Add button to update document's title

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,13 +1,17 @@
-import { doc, deleteDoc } from "firebase/firestore";
+import { useState } from "react";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 
 export const Movie = ({ db, id, title, receivedAnOscar, releaseDate }) => {
+    const [newTitle, setNewTitle] = useState("");
+
     const deleteMovie = async (id) => {
-        try {
-            const movieDoc = doc(db, "movies", id);
-            await deleteDoc(movieDoc);
-        } catch (err) {
-            console.error(err);
-        }
+        const movieDoc = doc(db, "movies", id);
+        await deleteDoc(movieDoc);
+    };
+
+    const updateTitle = async (id) => {
+        const movieDoc = doc(db, "movies", id);
+        await updateDoc(movieDoc, { title: newTitle });
     };
 
     return (
@@ -21,7 +25,14 @@ export const Movie = ({ db, id, title, receivedAnOscar, releaseDate }) => {
             </h1>
             <p>Date: {releaseDate}</p>
             <button onClick={() => deleteMovie(id)}>Delete</button>
-            <input type="text" placeholder="edit title..." />
+            <input
+                type="text"
+                placeholder="edit title..."
+                onChange={(e) => {
+                    setNewTitle(e.target.value);
+                }}
+            />
+            <button onClick={() => updateTitle(id)}>Update Title</button>
         </div>
     );
 };
