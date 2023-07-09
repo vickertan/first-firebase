6c149b18c109d576cf7fdc574c1d3e1dc07dd777
VickerTan <vickerpangestan@gmail.com>
2023-07-09 23:06:00 +0800

    Prevent rerendering entire app on changed state of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,20 @@
-import { useEffect, useState } from "react";
+import { collection } from "firebase/firestore";
 import { Auth } from "./components/Auth";
 import { MovieForm } from "./components/MovieForm";
 import { MovieList } from "./components/MovieList";
 import { db, auth } from "./config/firebase";
-import { getDocs, collection } from "firebase/firestore";
 
 const App = () => {
     console.log("Render App.");
-    const [movieList, setMovieList] = useState([]);
 
     const movieColl = collection(db, "movies");
-
-    useEffect(() => {
-        console.log("getMovieList");
-        const getMovieList = async () => {
-            try {
-                const data = await getDocs(movieColl);
-                const fData = data.docs.map((doc) => ({
-                    ...doc.data(),
-                    id: doc.id,
-                }));
-                setMovieList(fData);
-            } catch (err) {
-                console.error(err);
-            }
-        };
-
-        getMovieList();
-    }, []);
 
     return (
         <div>
             <h1>{auth?.currentUser?.email}</h1>
             <Auth />
             <MovieForm movieColl={movieColl} />
-            <MovieList movieList={movieList} db={db} />
+            <MovieList movieColl={movieColl} db={db} />
         </div>
     );
 };
