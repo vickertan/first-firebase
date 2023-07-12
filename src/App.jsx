88ace11c5e1a520945e8d3828adf5459b989e421
VickerTan <vickerpangestan@gmail.com>
2023-07-12 22:55:55 +0800

    Rerendering on user changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,29 @@ import { Auth } from "./components/Auth";
 import { MovieForm } from "./components/MovieForm";
 import { MovieList } from "./components/MovieList";
 import { db, auth } from "./config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 const App = () => {
     console.log("Render App.");
+    const [user, setUser] = useState(null);
 
     const movieColl = collection(db, "movies");
 
+    useEffect(() => {
+        onAuthStateChanged(auth, (u) => {
+            if (u) {
+                setUser(u);
+                console.log(u);
+            } else {
+                setUser(null);
+            }
+        });
+    }, []);
+
     return (
         <div>
-            <h1>{auth?.currentUser?.email}</h1>
+            <h1>{user ? user.email : "no user logged in"}</h1>
             <Auth />
             <MovieForm movieColl={movieColl} />
             <MovieList movieColl={movieColl} db={db} />
